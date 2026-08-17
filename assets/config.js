62a5c8040f82597b85332e101cd0a523cead2bbe
saputrarevolution2-cell <saputrarevolution2@gmail.com>
2026-08-18 02:43:14 +0700

    Delete assets directory

--- assets/config.js
+++
@@ -1,4 +0,0 @@
-window.TELECOD_CONFIG = {
-  SUPABASE_URL: "https://qrhbgffmqorzbcfvnbkk.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJxcmhiZ2ZmbXFvcnpiY2Z2bmJrayIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzg2OTUxMzkyLCJleHAiOjIxMDI1MjczOTJ9.W9tWYiPmYOC9wsruJMypH_Kg0dQpw_klCbACS6PYp48"
-};
--- assets/js/database.js
+++
@@ -1,5 +0,0 @@
-const SUPABASE_URL='https://qrhbgffmqorzbcfvnbkk.supabase.co';
-const SUPABASE_ANON_KEY='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJxcmhiZ2ZmbXFvcnpiY2Z2bmJrayIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzg2OTUxMzkyLCJleHAiOjIxMDI1MjczOTJ9.W9tWYiPmYOC9wsruJMypH_Kg0dQpw_klCbACS6PYp48';
-window.telecodSupabase=window.supabase?.createClient(SUPABASE_URL,SUPABASE_ANON_KEY,{auth:{persistSession:true,autoRefreshToken:true,detectSessionInUrl:true,storage:window.localStorage}});
-async function tcSession(){const {data}=await window.telecodSupabase.auth.getSession();return data.session||null}
-async function tcUser(){const {data}=await window.telecodSupabase.auth.getUser();return data.user||null}
