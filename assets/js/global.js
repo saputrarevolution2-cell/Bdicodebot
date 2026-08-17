1f63d49ef3393ea0808ba16f07065bda5daac08b
saputrarevolution2-cell <saputrarevolution2@gmail.com>
2026-08-18 00:57:38 +0700

    Delete assets/js/global.js

--- assets/js/global.js
+++
@@ -1,7 +0,0 @@
-
-/* TeleCod shared UI safety */
-document.addEventListener("DOMContentLoaded",()=>{
-  document.querySelectorAll("img").forEach(img=>{
-    if(!img.getAttribute("loading")) img.setAttribute("loading","lazy");
-  });
-});
