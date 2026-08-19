7e39e478282496692943cd32d287f00be7a6d59e
saputrarevolution2-cell <saputrarevolution2@gmail.com>
2026-08-19 19:15:11 +0700

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,6 @@
 window.TELECOD_CONFIG = {
   SUPABASE_URL: "https://qrhbgffmqorzbcfvnbkk.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJxcmhiZ2ZmbXFvcnpiY2Z2bmJrayIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzg2OTUxMzkyLCJleHAiOjIxMDI1MjczOTJ9.W9tWYiPmYOC9wsruJMypH_Kg0dQpw_klCbACS6PYp48",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFyaGJnZmZtcW9yemJjZnZuYmtrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY5NTEzOTIsImV4cCI6MjEwMjUyNzM5Mn0.W9tWYiPmYOC9wsruJMypH_Kg0dQpw_klCbACS6PYp48",
   TELEGRAM_BOT_USERNAME: "ZyxFidxBot",
   TELEGRAM_AUTH_FUNCTION: "/functions/v1/telegram-auth"
 };
