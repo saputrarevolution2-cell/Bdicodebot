8595750f815063da89cdf4f4b67bd534a1eb3b01
saputrarevolution2-cell <saputrarevolution2@gmail.com>
2026-08-25 19:07:37 +0700

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -9,6 +9,7 @@ SUPABASE_URL:"https://qrhbgffmqorzbcfvnbkk.supabase.co",
   SUPABASE_ANON_KEY:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFyaGJnZmZtcW9yemJjZnZuYmtrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY5NTEzOTIsImV4cCI6MjEwMjUyNzM5Mn0.W9tWYiPmYOC9wsruJMypH_Kg0dQpw_klCbACS6PYp48",
 
   MARKETPLACE_FUNCTION_URL:"https://qrhbgffmqorzbcfvnbkk.supabase.co/functions/v1/marketplace",
+  REGISTER_FUNCTION_URL:"https://qrhbgffmqorzbcfvnbkk.supabase.co/functions/v1/register-account",
 
   PAYMENT_CREATE_FUNCTION_URL:"https://qrhbgffmqorzbcfvnbkk.supabase.co/functions/v1/payment-create",
 
