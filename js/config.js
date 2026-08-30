cfad1b0eab08f4981da82452ea70a509cdaa4a63
saputrarevolution2-cell <saputrarevolution2@gmail.com>
2026-08-30 18:13:15 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,5 +4,5 @@
 */
 window.PASTELE_CONFIG = Object.freeze({
   SUPABASE_URL: 'https://qrhbgffmqorzbcfvnbkk.supabase.co',
-  SUPABASE_ANON_KEY: ''
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFyaGJnZmZtcW9yemJjZnZuYmtrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY5NTEzOTIsImV4cCI6MjEwMjUyNzM5Mn0.W9tWYiPmYOC9wsruJMypH_Kg0dQpw_klCbACS6PYp48'
 });
