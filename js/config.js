1e826ea00f39edf5a2ac0b071c97972f6f93689b
saputrarevolution2-cell <saputrarevolution2@gmail.com>
2026-09-09 02:07:00 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,6 +3,6 @@
  * Never put service_role / secret keys in this browser file.
  */
 window.PASTELE_CONFIG = Object.freeze({
-  SUPABASE_URL: 'https://YOUR-PROJECT.supabase.co',
-  SUPABASE_ANON_KEY: 'YOUR_SUPABASE_ANON_OR_PUBLISHABLE_KEY'
+  SUPABASE_URL: 'https://jxrndamvelqwhbcromye.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp4cm5kYW12ZWxxd2hiY3JvbXllIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg4ODIzNTIsImV4cCI6MjEwNDQ1ODM1Mn0.M8bqTbSadCPLdWORE769BVBt7hr0VcYfrIWmjHpnfXo'
 });
