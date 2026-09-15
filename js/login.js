836e28b7792c4831aaa6185b3808690c40e8bf95
saputrarevolution2-cell <saputrarevolution2@gmail.com>
2026-09-16 03:20:29 +0700

    Update login.js

--- js/login.js
+++ js/login.js
@@ -7,7 +7,7 @@
   "use strict";
 
   const SUPABASE_URL = "https://jxrndamvelqwhbcromye.supabase.co";
-  const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp4cm5kYW12ZWxlbXFiY3JvbXllIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg4ODIzNTIsImV4cCI6MjEwNDQ1ODM1Mn0.M8bqTbSadCPLdWORE769BVBt7hr0VcYfrIWmjHpnfXo";
+  const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp4cm5kYW12ZWxxd2hiY3JvbXllIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg4ODIzNTIsImV4cCI6MjEwNDQ1ODM1Mn0.M8bqTbSadCPLdWORE769BVBt7hr0VcYfrIWmjHpnfXo";
 
   const $ = id => document.getElementById(id);
   const step1=$("loginStep1"), step2=$("loginStep2"), identifier=$("identifier");
